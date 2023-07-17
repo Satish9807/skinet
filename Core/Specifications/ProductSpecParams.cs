895e41db2e536fc20703d689279df24e08ae3801
Satish9807 <satishshukla9807231294@gmail.com>
2023-07-17 21:00:07 +0530

    End of section 9

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Core.Specifications
 {
-    public class ProductSpecParams
+    public class  ProductSpecParams
     {
         private const int MaxPageSize = 50;
         public int PageIndex {get; set; } = 1;
